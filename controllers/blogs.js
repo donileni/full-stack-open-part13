7260999d2269aff8d29c217617bf2f8aed4e7d42
David <david.markus.olsson@gmail.com>
2025-07-31 09:52:58 +0200

    Finished 13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models")
+
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+
+router.get("/", async (req, res) => {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.json(blog)
+    } catch (error) {
+        return res.status(400).json({ error })
+    }
+})
+
+router.delete("/:id", blogFinder, async (req, res) => {
+    if (req.blog) {
+        await req.blog.destroy()
+        res.status(204).end()
+    } else {
+        res.status(404).json({ error: "Blog not found"})
+    }
+})
+
+module.exports = router
